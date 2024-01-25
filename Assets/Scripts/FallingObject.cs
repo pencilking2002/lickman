9a8ba9e8acf57974fa5ab4a86fb4f9d1b30ec3a8
elijahjudge <elijahgjudge@gmail.com>
2024-01-25 18:50:01 -0500

    URP working

---
+++ Assets/Scripts/FallingObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class FallingObject : MonoBehaviour
+{
+    public enum Type
+    {
+        good,
+        bad
+    }
+
+    [SerializeField] private float pointWorth;
+    [SerializeField] private Type type;
+    public virtual void OnLick()
+    {
+
+    }
+
+
+    public virtual float GetPointWorth() => pointWorth;
+    public Type GetFallingObjectType() => type;
+    
+}
