335329609351405aa1b614b9d8172171e40cb4fa
elijahjudge <elijahgjudge@gmail.com>
2024-01-25 16:43:52 -0500

    Body influences Head Z position

--- Assets/Scripts/MovementHead.cs
+++ Assets/Scripts/MovementHead.cs
@@ -21,6 +21,7 @@ public class MovementHead : MonoBehaviour
 
     [Header("Transforms")]
     public Transform neckConnectPoint;
+    public Transform body;
 
     [Header("Debug")]
     public float neckLength; // this variable will be changed outside this script
@@ -42,10 +43,20 @@ public class MovementHead : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(useArrowKeys)
+        UpdateInfinitePlaneAccordingToBodyZPosition();
+
+        if (useArrowKeys)
             MoveHeadUsingTranslation();
         else
             MoveHeadBasedOnMousePosition();
+    }
+
+    private void UpdateInfinitePlaneAccordingToBodyZPosition()
+    {
+        m_DistanceFromCamera = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, body.position.z);
+
+        //Create a new plane with normal (0,0,1) at the position away from the camera you define in the Inspector. This is the plane that you can click so make sure it is reachable.
+        infinitePlane = new Plane(Vector3.forward, m_DistanceFromCamera);
     }
 
     private void MoveHeadBasedOnMousePosition()
@@ -55,9 +66,9 @@ public class MovementHead : MonoBehaviour
     private void MoveHeadUsingTranslation()
     {
         Vector3 direction = new Vector3(ManagerInputSystem.instance.GetArrowKeyInput().x, ManagerInputSystem.instance.GetArrowKeyInput().y, 0f);
-
         transform.Translate(direction * speed * Time.deltaTime);
         transform.position = ClampPositionHalfCircle(transform.position, neckLength);
+        transform.position = new Vector3(transform.position.x, transform.position.y, body.position.z);
     }
 
     private Vector3 GetHeadGoalPosition(Vector3 currentPosition)
@@ -73,23 +84,22 @@ public class MovementHead : MonoBehaviour
             Vector3 hitPoint = ray.GetPoint(enter);
 
             //Move your cube GameObject to the point where you clicked
-            return ClampPositionHalfCircle(hitPoint,neckLength);
+            Vector3 movePosition = ClampPositionHalfCircle(hitPoint, neckLength);
+
+            return movePosition;
         }
 
-        return currentPosition;
+        return new Vector3(currentPosition.x,currentPosition.y,body.position.z);
     }
 
 
     private Vector3 ClampPositionHalfCircle( Vector3 position, float neckLength)
     {
-        Vector3 circleCenter = new Vector3(
-            neckConnectPoint.transform.position.x,
-            neckConnectPoint.transform.position.y,
-            0f );
+        Vector3 circleCenter = neckConnectPoint.transform.position;
 
         Vector3 circlePosition = (position - circleCenter).normalized * neckLength + circleCenter;
 
-        circlePosition = new Vector3(circlePosition.x,Mathf.Max(circlePosition.y, floorYClamp),0f);
+        circlePosition = new Vector3(circlePosition.x,Mathf.Max(circlePosition.y, floorYClamp),circlePosition.z);
 
         if (Vector3.Distance(position, circleCenter) > neckLength)
             return ClampYHeight(circlePosition);
@@ -100,6 +110,6 @@ public class MovementHead : MonoBehaviour
 
     private Vector3 ClampYHeight(Vector3 vector)
     {
-        return new Vector3(vector.x, Mathf.Max(vector.y, floorYClamp), 0f);
+        return new Vector3(vector.x, Mathf.Max(vector.y, floorYClamp), vector.z);
     }
 }
