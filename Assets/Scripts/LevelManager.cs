85080ae04679f6f27e80ea54fd3485f694837050
elijahjudge <elijahgjudge@gmail.com>
2024-01-25 19:41:25 -0500

    big number

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,7 +7,7 @@ public class LevelManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameManager.Instance.testFloat = 9843279812374901237490281379280374098;
+        //GameManager.Instance.testFloat = 9843279812374901237490281379280374098;
     }
 
     // Update is called once per frame
